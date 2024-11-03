5bad316d48d82c78323d5bd58548e61ce8361d0e
Scala Steward <scala_steward@virtuslab.com>
2024-11-03 18:15:00 +0000

    Update scala3-library to 3.3.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import Keys._
 object Dependencies {
   val scala212 = "2.12.18"
   val scala213 = "2.13.12"
-  val scala3 = "3.3.1"
+  val scala3 = "3.3.4"
 
   val scalaCompiler = Def.setting {
     val v = if (scalaBinaryVersion.value == "3") scala213 else scalaVersion.value
